671fef8b16ddb2be2e6cc25f86f2c41c943ec0d7
Epiq122 <l2ob122@gmail.com>
2022-04-16 22:57:02 -0700

    Added random pick functionality to computer

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,14 +9,33 @@ public class Main {
         System.out.println("Lets's play Rock Paper Scissors");
         System.out.println("When i say 'shoot', Choose: rock, paper, or scissors");
         System.out.println("Are you ready? Write 'yes' if you are");
+
+
         String ready = scan.nextLine();
 
         if(ready.equals("yes")){
             System.out.println("Great!");
             System.out.println("rock -- paper -- scissors, shoot!");
             String playerChoice = scan.nextLine();
+            System.out.println("\nYou chose:     " + playerChoice);
+            System.out.println("The computer chose: " + computerChoice());
         } else{
             System.out.println("You lost, better luck next time!");
         }
+    }
+
+    public static String computerChoice(){
+
+       double randomNumber = (int) Math.floor(Math.random() * 2);
+        String choice = null;
+        if(randomNumber == 0){
+            choice = "rock";
+        }else if(randomNumber == 1){
+            choice = "paper";
+        }else if(randomNumber == 3){
+            choice = "scissors";
+        }
+
+return choice;
     }
 }
